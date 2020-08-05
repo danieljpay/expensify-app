2f8cfd0c53cf7c1d349ad28ed81e2a72ef8075f7
Daniel Pérez <plupyt_dany@hotmail.com>
2020-08-05 18:13:52 -0500

    base

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp.js";
+import  "normalize.css/normalize.css";
+import "./styles/styles.css";
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
