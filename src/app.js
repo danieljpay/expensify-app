8f55d193d87e63c22e7e02c4c084a90da0a74135
Daniel Perez <plupyt_dany@hotmail.com>
2020-08-24 13:13:15 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses.js";
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from "./actions/filters.js";
+import { setTextFilter } from "./actions/filters.js";
 import getVisibleExpenses from "./selectors/expenses.js";
 import  "normalize.css/normalize.css";
 import "./styles/styles.css";
